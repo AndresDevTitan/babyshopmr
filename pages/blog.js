bb27c4a8f1a9e83998215175eb7dcd4505683a51
Andres Barreto <andres.barreto.garzon@hotmail.com>
2022-04-14 14:52:57 -0500

    Consumo de strapi y cloudinary por medio de getServerSideProps

--- pages/blog.js
+++ pages/blog.js
@@ -1,6 +1,8 @@
 import Layout from "../components/layout";
 
-export default function Blog() {
+export default function Blog({ result }) {
+    console.log(result);
+
     return (
         <div>
             <Layout pagina='Blog'>
@@ -8,4 +10,15 @@ export default function Blog() {
             </Layout>
         </div>
     );
+}
+
+export async function getServerSideProps() {
+    const url = 'http://localhost:1337/api/blogs?populate=*'
+    const response = await fetch(url);
+    const result = await response.json();
+    return {
+        props: {
+            result
+        }
+    }
 }
